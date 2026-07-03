824c802d851e1b5c154a1109a0a53cb49de99604
Deepanshu Singh <deepanshu.s@celestialsys.com>
2026-07-04 00:12:56 +0530

    add github actions for backend

--- Backend/server.ts
+++ Backend/server.ts
@@ -20,7 +20,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://socialevent-platform-client.onrender.com", //For Production
+      "https://socialevent-platform-client.onrender.com",
     ],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
